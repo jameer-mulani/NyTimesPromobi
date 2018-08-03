628ef329bf0e2036c187837c9caabbf010e550b2
Jameer Mulani <jameer.a.mulani@gmail.com>
2018-08-03 23:04:42 +0530

    1.Added livedata field and getter

--- app/src/main/java/com/assigment/nytimespromobi/facade/StoryViewModel.java
+++ app/src/main/java/com/assigment/nytimespromobi/facade/StoryViewModel.java
@@ -2,20 +2,27 @@ package com.assigment.nytimespromobi.facade;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import com.assigment.nytimespromobi.db.model.StoryEntity;
+
+import java.util.List;
 
 public class StoryViewModel extends AndroidViewModel {
 
     private Application mContext;
     private Repository mRepository;
+    private LiveData<List<StoryEntity>> mAllStories;
 
     public StoryViewModel(Application context) {
         super(context);
         this.mContext = context;
         mRepository = new Repository(mContext);
+        mAllStories = mRepository.getAllStories();
     }
 
-    public void getStories() {
-        mRepository.getStories();
-    }
 
+    public LiveData<List<StoryEntity>> getAllStories() {
+        return mAllStories;
+    }
 }
