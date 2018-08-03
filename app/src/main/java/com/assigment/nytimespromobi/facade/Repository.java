2f9a4752340057d0516954aefc98ae6d7e4bbcf1
Jameer Mulani <jameer.a.mulani@gmail.com>
2018-08-03 23:06:31 +0530

    1.Added implementation for fetching api from network and offline capability when necessary

--- app/src/main/java/com/assigment/nytimespromobi/facade/Repository.java
+++ app/src/main/java/com/assigment/nytimespromobi/facade/Repository.java
@@ -1,13 +1,26 @@
 package com.assigment.nytimespromobi.facade;
 
+import android.arch.lifecycle.LiveData;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.assigment.nytimespromobi.db.StoryDao;
+import com.assigment.nytimespromobi.db.StoryDatabase;
+import com.assigment.nytimespromobi.db.model.DatabaseHelper;
+import com.assigment.nytimespromobi.db.model.StoryEntity;
+import com.assigment.nytimespromobi.db.model.StoryMultimediaEntity;
 import com.assigment.nytimespromobi.network.StoryApiClient;
 import com.assigment.nytimespromobi.network.StoryApiService;
+import com.assigment.nytimespromobi.network.model.Story;
+import com.assigment.nytimespromobi.network.model.StoryMultimedia;
 import com.assigment.nytimespromobi.network.model.StoryResponse;
+import com.assigment.nytimespromobi.util.EntityBinder;
 import com.assigment.nytimespromobi.util.PreferenceUtil;
+
+import java.util.List;
 
 import io.reactivex.SingleObserver;
 import io.reactivex.disposables.Disposable;
@@ -20,22 +33,46 @@ public class Repository {
 
     private Context mContext;
     private StoryApiService mStoryApiService;
+    private StoryDao mStoryDao;
+    private LiveData<List<StoryEntity>> mAllStories;
 
 
     public Repository(Context context) {
         this.mContext = context;
         Retrofit retrofit = StoryApiClient.getApiClient();
         mStoryApiService = retrofit.create(StoryApiService.class);
+        StoryDatabase database = StoryDatabase.getInstance(context);
+        mStoryDao = database.getStoryDao();
+    }
+
+    private void fetchStories() {
+        mAllStories = new DatabaseHelper(mContext).getAllStories();
+        if (isNetworkAvailable()) {
+            fetchNetworkStories();
+        }
+    }
+
+    public LiveData<List<StoryEntity>> getAllStories() {
+        fetchStories();
+        return mAllStories;
     }
 
 
-    public void getStories() {
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void fetchNetworkStories() {
 
         String apiKey = PreferenceUtil.getStoryApiKey(mContext);
         if (!TextUtils.isEmpty(apiKey)) {
 
             mStoryApiService.fetchAllStories(apiKey)
                     .subscribeOn(Schedulers.io())
+                    .observeOn(Schedulers.io())
                     .subscribeWith(new SingleObserver<StoryResponse>() {
                         @Override
                         public void onSubscribe(Disposable d) {
@@ -45,8 +82,29 @@ public class Repository {
                         @Override
                         public void onSuccess(StoryResponse storyResponse) {
 
-                            Log.d(TAG, "response recieved:" + storyResponse.getResultCount());
+                            List<Story> results = storyResponse.getResults();
 
+                            if (results != null && !results.isEmpty()) {
+                                mStoryDao.clearAllStories();
+
+                                for (Story story : results) {
+
+                                    //insert story
+                                    long rowId = mStoryDao.insertStory(EntityBinder.bind(story));
+
+                                    for (StoryMultimedia multimedia : story.getMultimedia()) {
+
+                                        StoryMultimediaEntity entity = EntityBinder.bind(multimedia);
+                                        entity.setStoryId(rowId);
+
+                                        //insert multimedia
+                                        mStoryDao.insertStoryMultimedia(entity);
+                                    }
+                                }
+                            }
+
+
+                            Log.d(TAG, "response recieved:" + storyResponse.getResultCount());
                         }
 
                         @Override
