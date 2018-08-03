839da571de96c4d8f331f64fbd9aa93ce246ecf1
Jameer Mulani <jameer.a.mulani@gmail.com>
2018-08-03 23:05:22 +0530

    1.Added helper to class to convert entity from nw model

---
+++ app/src/main/java/com/assigment/nytimespromobi/util/EntityBinder.java
@@ -0,0 +1,42 @@
+package com.assigment.nytimespromobi.util;
+
+import android.support.annotation.NonNull;
+
+import com.assigment.nytimespromobi.db.model.StoryEntity;
+import com.assigment.nytimespromobi.db.model.StoryMultimediaEntity;
+import com.assigment.nytimespromobi.network.model.Story;
+import com.assigment.nytimespromobi.network.model.StoryMultimedia;
+
+public class EntityBinder {
+
+    private EntityBinder() {
+
+    }
+
+    public static StoryEntity bind(@NonNull Story nwStoryModel) {
+
+        StoryEntity storyEntity = new StoryEntity();
+        storyEntity.setByLine(nwStoryModel.getByline());
+        storyEntity.setCreatedDate(nwStoryModel.getCreatedDate());
+        storyEntity.setItemType(nwStoryModel.getItemType());
+        storyEntity.setPublishedDate(nwStoryModel.getPublishedDate());
+        storyEntity.setSection(nwStoryModel.getSection());
+        storyEntity.setStoryAbstract(nwStoryModel.getAbstractStory());
+        storyEntity.setTitle(nwStoryModel.getTitle());
+        storyEntity.setUpdatedType(nwStoryModel.getUpdatedDate());
+        storyEntity.setUrl(nwStoryModel.getUrl());
+        return storyEntity;
+    }
+
+    public static StoryMultimediaEntity bind(@NonNull StoryMultimedia storyMultimedia) {
+
+        StoryMultimediaEntity entity = new StoryMultimediaEntity();
+        entity.setCopyright(storyMultimedia.getCopyright());
+        entity.setFormat(storyMultimedia.getFormat());
+        entity.setHeight(storyMultimedia.getHeight());
+        entity.setWidth(storyMultimedia.getWidth());
+        entity.setType(storyMultimedia.getType());
+        return entity;
+    }
+
+}
